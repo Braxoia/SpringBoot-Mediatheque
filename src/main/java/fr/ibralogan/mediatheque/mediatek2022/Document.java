4dacf1986e76fbd7d115fa4ff3b8f0eb4d28ae30
Braxoia <71882015+Braxoia@users.noreply.github.com>
2022-02-24 21:53:29 +0100

    Mise à jour
    Ajout d'un système d'authentification via JWT (en cours)
    Ajout d'un gestionnaire de documents (pas complet)

--- src/main/java/fr/ibralogan/mediatheque/mediatek2022/Document.java
+++ src/main/java/fr/ibralogan/mediatheque/mediatek2022/Document.java
@@ -1,14 +1,5 @@
-// Jean-Fran�ois Brette 01/01/2022
-
 package fr.ibralogan.mediatheque.mediatek2022;
 
-/**
- * @author jfbrette
- *
- */
-public interface Document {
-	boolean disponible();
-	void emprunt(Utilisateur u) throws Exception;
-	void retour();
-	// l'affichage d'un document utilisera toString()
+public class Document {
+
 }
